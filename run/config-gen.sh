896985d12d89c9e0e286369b9b9ebecd85466b26
Christopher Kaster <me@atomicptr.de>
2025-12-05 13:50:46 +0100

    fix nginx client max body size not being properly set

--- run/config-gen.sh
+++ run/config-gen.sh
@@ -26,5 +26,8 @@ if [[ -n "${PHP_UPLOAD_MAX_FILESIZE+x}" ]]; then
     echo "    [boxed-php] ENV PHP_UPLOAD_MAX_FILESIZE found: $PHP_UPLOAD_MAX_FILESIZE -> PHP Setting: upload_max_filesize"
     echo "upload_max_filesize = $PHP_UPLOAD_MAX_FILESIZE" >>"$php_env_config"
     echo "    [boxed-php] ENV PHP_UPLOAD_MAX_FILESIZE found: $PHP_UPLOAD_MAX_FILESIZE -> NGINX Setting: client_max_body_size"
-    sed -i "s/client_max_body_size 10M/client_max_body_size ${PHP_UPLOAD_MAX_FILESIZE}/" /etc/nginx/http.d/default.conf
+    sed -i "s/client_max_body_size 1m/client_max_body_size ${PHP_UPLOAD_MAX_FILESIZE,,}/" /etc/nginx/nginx.conf
+else
+    # if user hasnt set it at least set it to our default value
+    sed -i "s/client_max_body_size 1m/client_max_body_size 10m/" /etc/nginx/nginx.conf
 fi
